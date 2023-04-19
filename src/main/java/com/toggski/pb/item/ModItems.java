0455a0431ef30b5a2041ba46f8c6d711a86b23e7
Toggski <toggski@gmail.com>
2023-04-19 16:06:26 +0100

    added items class

---
+++ src/main/java/com/toggski/pb/item/ModItems.java
@@ -0,0 +1,21 @@
+package com.toggski.pb.item;
+
+import com.toggski.pb.PaleontologyBeyond;
+import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, PaleontologyBeyond.MOD_ID);
+
+    public static final RegistryObject<Item> AMBER = ITEMS.register("amber", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
